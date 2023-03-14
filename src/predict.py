37141d9d3d0a4f22dc99a13609c47ba8f27efdf3
Vaibhav Vemula <vaibhav25vemula23@gmail.com>
2023-03-14 19:13:57 +0530

    YoloV5 DVC

--- src/predict.py
+++ src/predict.py
@@ -6,12 +6,12 @@ import extras.logger as logg
 sys.path.insert(0, 'model/yolov5')
 import val
 
-if len(sys.argv) != 4:
-    sys.stderr.write('Arguments error. Usage:\n')
-    sys.stderr.write(
-        '\tpython3 src/train.py data/split data/trained data/augmented\n'
-    )
-    sys.exit(1)
+# if len(sys.argv) != 4:
+#     sys.stderr.write('Arguments error. Usage:\n')
+#     sys.stderr.write(
+#         '\tpython3 src/train.py data/split data/trained data/augmented\n'
+#     )
+#     sys.exit(1)
 
 def yolov5Model():
     args = {k:v for e in params['yolov5'] for (k,v) in e.items()}
@@ -26,6 +26,6 @@ def main():
 if __name__ == "__main__":
     logger = logg.log("predict.py")
     params = yaml.safe_load(open('params.yaml'))
-    output = os.path.join(sys.argv[2],f"v{params['ingest']['dcount']}",'images')
-    os.makedirs(output, exist_ok=True)
+    # output = os.path.join(sys.argv[2],f"v{params['ingest']['dcount']}",'images')
+    # os.makedirs(output, exist_ok=True)
     main()
