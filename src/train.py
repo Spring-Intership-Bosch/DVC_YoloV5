43b66c363b95fb5eca9dca281f64b0c2c88824e8
Abhishek D <abhishekdinesh21042001@gmail.com>
2023-03-14 10:39:02 +0530

    Updated first 3 stages

--- src/train.py
+++ src/train.py
@@ -4,17 +4,17 @@ import yaml
 sys.path.insert(0, 'model/yolov5')
 import train
 
-if len(sys.argv) != 2:
+if len(sys.argv) != 4:
     sys.stderr.write('Arguments error. Usage:\n')
     sys.stderr.write(
-        '\tpython3 src/train.py data/trained\n'
+        '\tpython3 src/train.py data/split data/trained data/augmented\n'
     )
     sys.exit(1)
 
 params = yaml.safe_load(open('params.yaml'))['ingest']
 
-output = os.path.join(sys.argv[1],f"v{params['dcount']}",'images')
+output = os.path.join(sys.argv[2],f"v{params['dcount']}",'images')
 os.makedirs(output, exist_ok=True)
 
 
-train.run(data='coco128.yaml', imgsz=320, weights='yolov5m.pt', epochs=1, batch=16)
+train.run(data='person.yaml', imgsz=640, weights='yolov5s.pt', epochs=1, batch=16)
