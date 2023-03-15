1e5febddb916570dc6703c5b00388bc35a681ea8
NikhilAdyapak <nikhiladyapak31@gmail.com>
2023-03-15 20:35:32 +0530

    update

--- main.py
+++ main.py
@@ -39,30 +39,7 @@ def main():
                 st.success('Pipeline executed successfully')
             else:
                 st.error("Pipleine execution failed")
-    
-    # if imgs:
-    #     params = yaml.safe_load(open('params.yaml'))['ingest']
-    #     ddd = {'ingest': {'dcount': params['dcount']+1}}
-    #     yaml.dump(ddd, open('params.yaml', 'w'))
-        
-    #     os.makedirs('buffer', exist_ok=True)
-    #     for ff in os.listdir('buffer'):
-    #         os.remove(f'buffer/{ff}')
-    
-    #     with open(f'buffer/dataset{params["dcount"]+1}.zip', "wb") as f:
-    #         f.write(imgs.getbuffer())
-        
-    #     print('hello................', sys.executable)
-        
-        # if not os.system("dvc repro"):
-        #     st.success('Pipeline executed successfully')
-        #     imgname = os.listdir("data/store/v{}/evaluated".format(params["dcount"]+1))
-        #     preds = glob.glob("data/store/v{}/evaluated/*.*".format(params["dcount"]+1), recursive=True)
-        #     for index,im in enumerate(preds):
-        #         st.image(im, imgname[index])
-        #     print('done')
-    # else:
-    #     return
+
 
 if __name__ == '__main__':
     params = yaml.safe_load(open('params.yaml'))
